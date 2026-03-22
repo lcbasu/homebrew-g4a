3b18f5820e56ae0b3037ba28466afbe33aa50c35
Lokesh Basu <lokesh.basu@gmail.com>
2026-03-22 15:25:58 +0530

    Add g4a formula

---
+++ Formula/g4a.rb
@@ -0,0 +1,24 @@
+class G4a < Formula
+  include Language::Python::Virtualenv
+
+  desc "The reasoning layer for AI-written code"
+  homepage "https://www.git4aiagents.com"
+  url "https://github.com/lcbasu/git4aiagents.git", branch: "master"
+  version "0.1.0"
+  license "CC-BY-4.0"
+
+  depends_on "python@3.13"
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/b9/2e/0090cbf739cee7d23781ad4b89a9894a41538e4fcf4c31dcdd705b78eb8b/click-8.1.8.tar.gz"
+    sha256 "ed53c9d8990d83c2a27deae68e4ee337473f6330c040a31d4225c9574d0c3e70"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    system bin/"g4a", "--version"
+  end
+end
